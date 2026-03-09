3e982b58b341a5c7367ffb65313dca4e2c56559f
honeybee <christi.lee0298@gmail.com>
2026-03-09 17:59:50 -0500

    updating file path for logo png

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,7 @@ export function Navbar() {
         <div className="flex items-center justify-between h-20">
           <Link href="/" className="flex items-center space-x-3 group">
             <Image
-              src="/images/logo.png"  
+              src="/images/logo.PNG"  
               alt="337 SLEDS & CUSTOMS Logo"
               width={120}                    
               height={40}                    
